c6b041df304dfe4519c68f8ae83177f20aecac97
Carlo de C. Navarro Álvarez <carnavalv@gmail.com>
2013-06-27 19:58:00 +0100

    Clase para comprobaciones de inicio
    Signed-off-by: Carlo de C. Navarro Álvarez <carnavalv@gmail.com>

---
+++ InfosierrAPP/src/es/ulpgc/IST/infosierrapp/main/Inicio.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package es.ulpgc.IST.infosierrapp.main;
+
+/**
+ * 
+ * Clase que se cargará siempre la primera cuando se inicie la
+ * App.
+ * 
+ * Realiza todas las comprobaciones de inicio que sean necesarias
+ * ej: si hay conexión a internet, si es datos o wifi ...
+ * 
+ * @author krlo
+ *
+ */
+public class Inicio {
+
+}
