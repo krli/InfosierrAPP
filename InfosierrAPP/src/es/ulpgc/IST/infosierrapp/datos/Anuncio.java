71951bc63d897dc2800de25a7389da0aad7e78c0
Carlo de C. Navarro Álvarez <carnavalv@gmail.com>
2013-07-11 14:03:08 +0100

    Corregida llamada repetida en Anuncio a set_Alltelefs...
    Signed-off-by: Carlo de C. Navarro Álvarez <carnavalv@gmail.com>

--- InfosierrAPP/src/es/ulpgc/IST/infosierrapp/datos/Anuncio.java
+++ InfosierrAPP/src/es/ulpgc/IST/infosierrapp/datos/Anuncio.java
@@ -221,10 +221,7 @@ public class Anuncio implements Serializable {
 		}
 	}
 	public void set_AllTelefonos(String tlfs) {
-
-		set_telefonos(splitString(tlfs, N_TLFS));		
-		set_telefonos( splitString(tlfs, N_TLFS) );		
-
+		set_telefonos( splitString(tlfs, N_TLFS) );
 	}
 	//***********************************
 	public String get_email() {
