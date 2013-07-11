401827dcd3446a1dbd05d0577168cc0c1df35d51
Jesús <yesusrr@hotmail.com>
2013-07-11 14:06:25 +0100

    Trabajos con GoogleMaps

--- InfosierrAPP/src/es/ulpgc/IST/infosierrapp/maestrodetalle/ItemPresenterH.java
+++ InfosierrAPP/src/es/ulpgc/IST/infosierrapp/maestrodetalle/ItemPresenterH.java
@@ -2,6 +2,13 @@ package es.ulpgc.IST.infosierrapp.maestrodetalle;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import es.ulpgc.IST.infosierrapp.R;
 
 /**
@@ -10,8 +17,23 @@ import es.ulpgc.IST.infosierrapp.R;
  *
  */
 public class ItemPresenterH extends ItemPresenterV {
-	
+	private GoogleMap 		mMap;
 
+	protected void onCreate(){
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		
+		
+		
+		LatLng posicion = new LatLng(X, Y);
+		mMap.addMarker(new MarkerOptions().position(posicion).title(this.nombre_string));
+		// Mueve la camara a la posicion
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posicion, 15));
+		// Anima la camara.
+		mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);	
+		
+	}
+	
     protected void checkOrientation() {      
       Configuration config = getResources().getConfiguration();
       if (config.orientation != Configuration.ORIENTATION_LANDSCAPE) {
